c0ae740e3642f9573feaa4f49bb6a8cc45a54280
Ccanchillo <brunomixan@gmail.com>
2025-09-03 11:39:21 -0500

    Implementación de Django 4.2.6

---
+++ biblioteca/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'available')
+    list_filter = ('available', 'published_date')
+    search_fields = ('title', 'author')
+
+# Register your models here.
---
+++ biblioteca/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+from .forms import BookForm
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'biblioteca/book_list.html', {'books': books})
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'biblioteca/book_form.html', {'form': form, 'title': 'Agregar Libro'})
+
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'biblioteca/book_form.html', {'form': form, 'title': 'Editar Libro'})
+
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'biblioteca/book_confirm_delete.html', {'book': book})
+
+def toggle_availability(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book.available = not book.available
+    book.save()
+    return redirect('book_list')
+
+# Create your views here.
